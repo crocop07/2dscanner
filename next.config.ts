89d93ed14c0471aa1b0de7dcafa5cca97eaac637
Andrew Roberts <crocop07@gmail.com>
2026-09-10 20:52:59 +0100

    update next config

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // Required for static exports on GitHub Pages
   },
-  // basePath: '/2dscanner', // ONLY uncomment if hosted at username.github.io/2dscanner/ without a custom domain
+  basePath: '/2dscanner', // ONLY uncomment if hosted at username.github.io/2dscanner/ without a custom domain
 };
 
 export default nextConfig;
